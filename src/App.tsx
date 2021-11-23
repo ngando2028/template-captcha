1f4acce3ce7f240bd2bd3cb2b08983cd0e1c2c2d
ngando2028 <ngan.do@zodinet.tech>
2021-11-23 10:41:48 +0700

    update site key

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "antd/dist/antd.css";
 import "./App.css";
-import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3';
+import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3";
 import FormLogin from "./Login/FormLogin";
 
 function App() {
@@ -9,8 +9,8 @@ function App() {
 		<div className="App">
 			<GoogleReCaptchaProvider
 				useRecaptchaNet
-				reCaptchaKey="6Ldsfk8dAAAAAA8KoF9rJ5Hix3hsqxxRi55OnSh9"
-				scriptProps={{ async: true, defer: true, appendTo: 'body' }}
+				reCaptchaKey="6LcJCk8dAAAAAHHaH_kRwhK9_m-xG0OiNdTz6B2N"
+				scriptProps={{ async: true, defer: true, appendTo: "body" }}
 			>
 				<FormLogin />
 			</GoogleReCaptchaProvider>
